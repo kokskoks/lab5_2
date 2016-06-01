1f637af0e33c79e9d125202b6aee5d1377e1785a
Kadłubek <179640@edu.p.lodz.pl>
2016-06-01 23:17:32 +0200

    Added depndencies and implemented ApplicationConfig class

--- src/main/java/edu/iis/mto/integrationtest/config/ApplicationConfig.java
+++ src/main/java/edu/iis/mto/integrationtest/config/ApplicationConfig.java
@@ -1,8 +1,25 @@
 package edu.iis.mto.integrationtest.config;
 
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
 
+import edu.iis.mto.integrationtest.utils.ModeUtils;
 
+@Configuration //wskazuje, ¿e klasa zawiera konfiguracjê bean’ów Spring
+@ComponentScan("edu.iis.mto.integrationtest.repository") //ustala kontekst poszukiwania komponentów
+@Import(value = { PersistenceConfig.class }) //klasa zwi¹zana z konfiguracj¹ bean’ów, która importujemy
 public class ApplicationConfig {
-
+	
+	@Bean
+	public static PropertyPlaceholderConfigurer getPropertyPlaceHolderConfigurer(){
+		PropertyPlaceholderConfigurer resolver = new PropertyPlaceholderConfigurer();
+		resolver.setLocation(new ClassPathResource(ModeUtils.getMode().getModeName() + 
+				"-persistence.properties")); 
+		return resolver;
+	}
 	
 }

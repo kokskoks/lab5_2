24b4b6f3a56a9f8cd14a8b01c0d56d2d1b88cd97
Kadłubek <179640@edu.p.lodz.pl>
2016-06-01 23:57:06 +0200

    created crud test

--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -1,12 +1,13 @@
 package edu.iis.mto.integrationtest.repository;
 
 import static edu.iis.mto.integrationtest.repository.PersonBuilder.person;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.AssertThrows;
 import org.springframework.test.annotation.DirtiesContext;
 
 import edu.iis.mto.integrationtest.model.Person;
@@ -32,6 +33,48 @@ public class PersonRepositoryIntegrationTest extends IntegrationTest {
 		assertEquals("Mancini", personRepository.findOne(count + 1)
 				.getLastName());
 	}
+	
+	@Test 
+	public void deletePerson(){
+		long personId = 1;
+		
+		if(personRepository.exists(personId)){
+			personRepository.delete(personId);
+		} else {
+			fail("bad input data");
+		}
+		
+		assertEquals(personRepository.exists(personId), false);
+	}
+	
+	@Test
+	public void updatePerson(){
+		
+		long personId = 1;
+		
+		Person person = personRepository.findOne(personId);
+		
+		String oldFirstName = person.getFirstName();
+		String newFirstName = "Jan";
+		
+		if(newFirstName.equals(oldFirstName)){
+			fail("names are the same, cannot verify updateing");
+		}
+		
+		person.setFirstName(newFirstName);
+		
+		personRepository.saveAndFlush(person);
+		
+		Person modifiedPerson = personRepository.findOne(personId);
+		
+		assertEquals( newFirstName, modifiedPerson.getFirstName());
+		
+		
+	}
+	
+	
+	
+	
 
 	private Person a(PersonBuilder builder) {
 		return builder.build();

5650595e95ba30992d67f54b0f36249acaa4baa0
Kadłubek <179640@edu.p.lodz.pl>
2016-06-01 23:35:20 +0200

    extended interface with JpaRepository

--- src/main/java/edu/iis/mto/integrationtest/repository/PersonRepository.java
+++ src/main/java/edu/iis/mto/integrationtest/repository/PersonRepository.java
@@ -1,8 +1,10 @@
 package edu.iis.mto.integrationtest.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import edu.iis.mto.integrationtest.model.Person;
 
-public interface PersonRepository {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 
 
 }
